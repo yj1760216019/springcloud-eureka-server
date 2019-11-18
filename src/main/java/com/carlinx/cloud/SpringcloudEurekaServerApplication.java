3cce3ab88021f3b6de046e2c60044dfe3e9ba8dc
xxxx <1760216019@qq.com>
2019-11-18 09:41:10 +0800

    springcloud注册中心

---
+++ src/main/java/com/carlinx/cloud/SpringcloudEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.carlinx.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer         //表明该项目是eureka注册中心
+public class SpringcloudEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudEurekaServerApplication.class, args);
+    }
+
+}
